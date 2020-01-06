163bdce329b10bf28c5960159bd3ef120911808e
Troysterr <40834126+Troysterr@users.noreply.github.com>
2020-01-05 22:06:58 -0700

    Test

--- MovieListCreator/Program.cs
+++ MovieListCreator/Program.cs
@@ -66,7 +66,7 @@ namespace MovieListCreator {
         readFolder(sDirNow, movieListXml);
       }
 
-      // Remove any template category elements with 0 movies
+      // Remove any template category elements with 0 movies...
       foreach (XmlNode ndEmpty in movieListXml.SelectNodes("//category[@count=\"0\"]")) {
         ndEmpty.ParentNode.RemoveChild(ndEmpty);
       }
